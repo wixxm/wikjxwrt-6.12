cbf57e25f04380445903bbc3437ad7284c658023
wixxm <94870224+wixxm@users.noreply.github.com>
2025-04-21 11:42:53 +0800

    Update 02-prepare_package.sh

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -18,11 +18,6 @@ git clone https://github.com/sbwml/wwan-packages package/new/wwan
 rm -rf feeds/luci/applications/luci-app-filemanager
 git clone https://$github/sbwml/luci-app-filemanager package/new/luci-app-filemanager
 
-# luci-app-airplay2
-git clone https://github.com/sbwml/luci-app-airplay2 package/new/airplay2
-
-# luci-app-webdav
-git clone https://$github/sbwml/luci-app-webdav package/new/luci-app-webdav
 
 # ddns - fix boot
 sed -i '/boot()/,+2d' feeds/packages/net/ddns-scripts/files/etc/init.d/ddns
@@ -84,49 +79,12 @@ sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/smb.conf.te
 # rk3568 bind cpus
 [ "$platform" = "rk3568" ] && sed -i 's#/usr/sbin/smbd -F#/usr/bin/taskset -c 1,0 /usr/sbin/smbd -F#' feeds/packages/net/samba4/files/samba.init
 
-# zerotier
-rm -rf feeds/packages/net/zerotier
-git clone https://$github/sbwml/feeds_packages_net_zerotier feeds/packages/net/zerotier
-
-# aria2 & ariaNG
-rm -rf feeds/packages/net/ariang
-rm -rf feeds/luci/applications/luci-app-aria2
-git clone https://$github/sbwml/ariang-nginx package/new/ariang-nginx
-rm -rf feeds/packages/net/aria2
-git clone https://$github/sbwml/feeds_packages_net_aria2 -b 22.03 feeds/packages/net/aria2
-
-# airconnect
-git clone https://$github/sbwml/luci-app-airconnect package/new/airconnect
-
 # netkit-ftp
 git clone https://$github/sbwml/package_new_ftp package/new/ftp
 
 # SSRP & Passwall
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 git clone https://$github/sbwml/openwrt_helloworld package/new/helloworld -b v5
-
-# alist
-rm -rf feeds/packages/net/alist feeds/luci/applications/luci-app-alist
-git clone https://$github/sbwml/openwrt-alist package/new/alist
-
-# netdata
-sed -i 's/syslog/none/g' feeds/packages/admin/netdata/files/netdata.conf
-
-# qBittorrent
-git clone https://$github/sbwml/luci-app-qbittorrent package/new/qbittorrent
-
-# unblockneteasemusic
-git clone https://$github/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/new/luci-app-unblockneteasemusic
-sed -i 's/解除网易云音乐播放限制/网易云音乐解锁/g' package/new/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
-
-# Theme
-git clone --depth 1 https://$github/sbwml/luci-theme-argon package/new/luci-theme-argon
-
-# Mosdns
-git clone https://$github/sbwml/luci-app-mosdns -b v5 package/new/mosdns
-
-# OpenAppFilter
-git clone https://$github/sbwml/OpenAppFilter --depth=1 package/new/OpenAppFilter
 
 # iperf3
 sed -i "s/D_GNU_SOURCE/D_GNU_SOURCE -funroll-loops/g" feeds/packages/net/iperf3/Makefile
@@ -170,8 +128,6 @@ git clone https://$github/sbwml/feeds_packages_utils_unzip feeds/packages/utils/
 # tcp-brutal
 git clone https://$github/sbwml/package_kernel_tcp-brutal package/kernel/tcp-brutal
 
-# watchcat - clean config
-true > feeds/packages/utils/watchcat/files/watchcat.config
 
 # libpcap
 rm -rf package/libs/libpcap
